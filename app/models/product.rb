4d873cb975872999a923e7e05c418b36fafe4034
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2016-02-15 10:33:12 -0500

    Add the Product and Category models

---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  price      :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Product < ActiveRecord::Base
+  has_and_belongs_to_many :categories
+end
